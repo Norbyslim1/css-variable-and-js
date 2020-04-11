a3e4719bf4f064ae1eba79a4e31c94b02881a6f4
Norbyslim1 <osubornorbert@gmail.com>
2020-04-11 11:32:08 +0100

    added retraction of window and respective sounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,28 @@
 $(function(){
+    $('#file').on('change', retract);
+    $('#file').on('click', close);
     $('button').on('click', startShow);
     function startShow(event){
         if ($('#file').val()==''){
             alert('please choose a file');
         }else{
-            var reel= URL.createObjectURL(event.target.files[0]);
-        $('#block-container').fadeIn('fast').css('background-image', 'url('+reel+')');
-            
+            $('#leftpane').removeClass('withdraw');
+            $('#rightpane').removeClass('withdraw'); 
+            var player= document.getElementById('bang');
+            player.currentTime=0;
+            player.play();
         }
     }
-    //     console.log(event.target);
-    //     file=URL.createObjectURL(event.target.files[0]);
-    //     $('img').fadeIn('fast').attr('src',file);
-        
-    //     }
-    // // }
-    // $('#file').change(function(event){
-    //     var reel= URL.createObjectURL(event.target.files[0]);
-    //     $('#block-container').fadeIn('fast').css('background-image', 'url('+reel+')');
-        
-    // })
+    function retract (event){
+        console.log('in withdraw');
+        var reel= URL.createObjectURL(event.target.files[0]);
+        $('#block-container').fadeIn('fast').css('background-image', 'url('+reel+')');
+        $('#leftpane').addClass('withdraw');
+        $('#rightpane').addClass('withdraw');
+    }
+    function close (event){
+        $('#leftpane').removeClass('withdraw');
+        $('#rightpane').removeClass('withdraw');
+
+    }
 })
